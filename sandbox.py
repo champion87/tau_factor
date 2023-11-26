0ef4f2f6bf67d568892b5d458ca56ef64a3f3b72
champion8 <lidor071205@gmail.com>
2023-11-26 23:00:26 +0200

    improve of 'request' function and a new 'simplify' function

--- sandbox.py
+++ sandbox.py
@@ -5,25 +5,37 @@ import requests
 TAU_FACTOR_SERVER_URL = "https://www.tau-factor.com/api/v1/courses/"
 REQUEST_BY_CODE_PARAM = "?course_code="
 
+DEFAULT_FIELDS = ["year", "semester", "statistics"]
+
 HEDVA_CODE = "0366-1101"
-    
-    
-def request(code: str):
-    request_url = TAU_FACTOR_SERVER_URL + REQUEST_BY_CODE_PARAM + code
+def request(course_code: str) ->dict:
+    request_url = TAU_FACTOR_SERVER_URL + REQUEST_BY_CODE_PARAM + course_code
     print(f"requesting from {request_url}:")
     response = requests.get(request_url)
+    return response.json()
     
+def request_and_save(code: str) -> None:    
     with open(f"{code}.json", "w") as dump_me:
-        response_json = response.json()
-        json.dump(response_json, dump_me, indent=4)
+        json.dump(request(code), dump_me, indent=4)
 
 def main():
-    request(HEDVA_CODE)
+    hedva = request(HEDVA_CODE)
+    hedva_simp = simplify(hedva)
+    with open(f"hedva_simp.json", "w") as dump_me:
+        json.dump(hedva_simp, dump_me, indent=4)
     
-    with open(f"{HEDVA_CODE}.json", encoding = "utf-8") as my_json:
-        json_dict = json.load(my_json)
-        data = json_dict["results"]    
     
+    
+def simplify(course_details: dict, fields=DEFAULT_FIELDS):
+    results = course_details["results"][0]
+    instances = results["instances"]
+    res = []
+    for inst in instances:
+        simplified = {k:v for k,v in inst.items() if k in fields}
+        res.append(simplified)
+    return res
+    
+
 if __name__ == "__main__":
     main()
     
